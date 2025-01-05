f86018fe1316b43b8077d55d566fc9d22be084b7
Artem Shlagin <3321301+shlajin@users.noreply.github.com>
2025-01-05 00:43:13 -0500

    Initial silly implementation

---
+++ src/extension.ts
@@ -0,0 +1,266 @@
+import path from "path";
+import * as vscode from "vscode";
+import { GitExtension, Repository, Status } from "./vendor/git";
+
+const colorsForCircles = [
+  "red",
+  "green",
+  "orange",
+  "blue",
+  "gray",
+  "transparent",
+] as const;
+
+const colorCircles = Object.fromEntries(
+  colorsForCircles.map((color) => [
+    color,
+    vscode.Uri.parse(
+      path.join(__dirname, "..", "media", `circle-${color}.svg`)
+    ),
+  ])
+) as Record<(typeof colorsForCircles)[number], vscode.Uri>;
+
+const colorsForMinuses = ["red"];
+const colorMinuses = Object.fromEntries(
+  colorsForMinuses.map((color) => [
+    color,
+    vscode.Uri.parse(path.join(__dirname, "..", "media", `minus-${color}.svg`)),
+  ])
+) as Record<(typeof colorsForMinuses)[number], vscode.Uri>;
+
+const iconsMap: Record<Status | -1, vscode.Uri> = {
+  [-1]: colorCircles.transparent,
+  [Status.INDEX_MODIFIED]: colorCircles.blue,
+  [Status.INDEX_ADDED]: colorCircles.green,
+  [Status.INDEX_DELETED]: colorCircles.gray,
+  [Status.INDEX_RENAMED]: colorCircles.blue,
+  [Status.INDEX_COPIED]: colorCircles.green,
+  [Status.MODIFIED]: colorCircles.blue,
+  [Status.DELETED]: colorCircles.gray,
+  [Status.UNTRACKED]: colorCircles.orange,
+  [Status.IGNORED]: colorCircles.gray,
+  [Status.INTENT_TO_ADD]: colorCircles.transparent,
+  [Status.INTENT_TO_RENAME]: colorCircles.transparent,
+  [Status.TYPE_CHANGED]: colorCircles.green,
+  [Status.ADDED_BY_US]: colorCircles.red,
+  [Status.ADDED_BY_THEM]: colorCircles.red,
+  [Status.DELETED_BY_US]: colorCircles.red,
+  [Status.DELETED_BY_THEM]: colorCircles.red,
+  [Status.BOTH_ADDED]: colorCircles.red,
+  [Status.BOTH_DELETED]: colorCircles.red,
+  [Status.BOTH_MODIFIED]: colorCircles.red,
+};
+
+const activateGit = async () => {
+  const iconsExtension = vscode.extensions.getExtension(
+    "vscode-icons-team.vscode-icons"
+  );
+  await iconsExtension?.activate();
+  iconsExtension?.exports;
+
+  const gitExtension =
+    vscode.extensions.getExtension<GitExtension>("vscode.git");
+
+  if (!gitExtension) {
+    vscode.window.showInformationMessage(`Can't get git extension`);
+    return;
+  }
+  const gitExports = await gitExtension.activate();
+  const gitAPI = gitExports.getAPI(1);
+
+  const currentWorkspace = vscode.workspace.workspaceFolders?.[0];
+  if (!currentWorkspace) {
+    vscode.window.showInformationMessage(`No workspace directories found`);
+    return;
+  }
+
+  await gitAPI.openRepository(currentWorkspace.uri);
+
+  if (gitAPI.repositories.length === 0) {
+    vscode.window.showInformationMessage(`Couldn't find any git repositories`);
+    return;
+  }
+
+  const repository = gitAPI.repositories[0];
+
+  return repository;
+
+  // const filePath = editor.document.uri;
+
+  // var repository = gitAPI.repositories.filter((r) =>
+  //   r.rootUri.fsPath.replace(/\\/g, "/").startsWith(this.g_workspaceFolder)
+  // )[0];
+  // if (!repository) {
+  //   vscode.window.showErrorMessage(
+  //     `Couldn't find a git repository at ${this.g_workspaceFolder} try to open VSCode with a different folder`
+  //   );
+  //   return;
+  // }
+  // const unstaged = repository.state.workingTreeChanges.map((r) => r.resource);
+  // const staged = repository.state.indexChanges.map((r) => r.resource);
+  // const all = unstaged.concat(staged);
+};
+
+const getFileStatuses = (repository: Repository) => {
+  const map = new Map<string, Status>();
+  repository.state.workingTreeChanges.forEach((r) => {
+    map.set(r.uri.fsPath, r.status);
+  });
+  repository.state.indexChanges.forEach((r) => {
+    map.set(r.uri.fsPath, r.status);
+  });
+
+  return map;
+};
+
+const isFileMarkedAsExcluded = (fullPath: string) => {
+  return fullPath.includes(path.sep + "node_modules" + path.sep);
+};
+
+declare module "vscode" {
+  interface QuickPickItem {
+    custom: {
+      fullPath: string;
+    };
+  }
+}
+
+const getQuickPickItemFromFilePath = (
+  fullPath: string,
+  status: Status | undefined
+): vscode.QuickPickItem => {
+  let iconPath: vscode.Uri;
+
+  const workspacePath = vscode.workspace.workspaceFolders![0].uri.path;
+  const description = path.dirname(fullPath).slice(workspacePath.length + 1);
+
+  if (isFileMarkedAsExcluded(fullPath)) {
+    iconPath = colorMinuses.red;
+  } else {
+    iconPath = iconsMap[status || -1];
+  }
+  return {
+    label: `$(file) ${path.basename(fullPath)}`,
+    description: description.length === 0 ? "" : description,
+    custom: {
+      fullPath,
+    },
+    iconPath,
+  };
+};
+
+const registerNewPath = (path: string, context: vscode.ExtensionContext) => {
+  const openedFiles = context.globalState.get("openedFiles", [] as string[]);
+
+  const index = openedFiles.indexOf(path);
+  if (index > -1) {
+    openedFiles.splice(index, 1);
+  }
+  openedFiles.unshift(path);
+
+  if (openedFiles.length >= 20) {
+    openedFiles.pop();
+  }
+
+  context.globalState.update("openedFiles", openedFiles);
+};
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export async function activate(context: vscode.ExtensionContext) {
+  // const gitColors = ['gitDecoration.addedResourceForeground',
+  //   'gitDecoration.modifiedResourceForeground',
+  //   'gitDecoration.deletedResourceForeground',
+  //   'gitDecoration.renamedResourceForeground',
+  //   'gitDecoration.stageModifiedResourceForeground',
+  //   'gitDecoration.stageDeletedResourceForeground',
+  //   'gitDecoration.untrackedResourceForeground',
+  //   'gitDecoration.ignoredResourceForeground',
+  //   'gitDecoration.conflictingResourceForeground',
+  //   'gitDecoration.submoduleResourceForeground',
+  //   'git.blame.editorDecorationForeground']
+
+  // const color = ;
+
+  //     gitColors.forEach(color => {
+  // console.log(color, new vscode.ThemeColor('addedResourceForeground'))
+  //     })
+  const repository = await activateGit();
+
+  if (!repository) {
+    return;
+  }
+
+  const onDidChangeVisible = vscode.window.onDidChangeActiveTextEditor(
+    (editor) => {
+      if (!editor) {
+        return;
+      }
+      registerNewPath(editor.document.uri.path, context);
+    }
+  );
+
+  context.subscriptions.push(onDidChangeVisible);
+  // context.subscriptions.push(onDidOpenTextDocument);
+
+  const disposable = vscode.commands.registerCommand(
+    "recently-used.list",
+    () => {
+      const workspaceFolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+      if (!workspaceFolder) {
+        vscode.window.showInformationMessage(
+          `Can't determine workspace folders`
+        );
+        return;
+      }
+
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      const openedFiles =
+        context.globalState.get<string[]>("openedFiles") || [];
+
+      const fileStatuses = getFileStatuses(repository);
+      const recentPicks = openedFiles.map((fullpath, index) =>
+        getQuickPickItemFromFilePath(fullpath, fileStatuses.get(fullpath))
+      );
+
+      const quickPick = vscode.window.createQuickPick<vscode.QuickPickItem>();
+      quickPick.title = "Select File";
+
+      quickPick.items = recentPicks;
+
+      quickPick.activeItems = [recentPicks[1]];
+
+      quickPick.onDidAccept(async () => {
+        const selected = quickPick.selectedItems[0];
+        if (selected) {
+          quickPick.dispose();
+
+          const fullPath = path.join(
+            ...[workspaceFolder, selected.description, selected.label].filter(
+              (x): x is string => Boolean(x)
+            )
+          );
+
+          try {
+            vscode.commands.executeCommand(
+              "vscode.open",
+              vscode.Uri.file(selected.custom.fullPath)
+            );
+          } catch (error) {
+            vscode.window.showErrorMessage(`Failed to open document: ${error}`);
+          }
+        }
+      });
+
+      quickPick.onDidHide(() => quickPick.dispose());
+      quickPick.show();
+      //   vscode.window.showInformationMessage("fuck you");
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
---
+++ src/generate-circles.ts
@@ -0,0 +1,42 @@
+import path from "path";
+import fs from "fs";
+
+const substr = `FILL_PLACEHOLDER`;
+
+const generatedMapCircles = {
+  red: "#ee3333", // weirdo
+  green: "#829260", // added
+  blue: "#a8c5f7", // edited
+  orange: "#DE8E88", // untracked
+  gray: "#888888", // ignored
+  transparent: "",
+};
+
+for (const [color, name] of Object.entries(generatedMapCircles)) {
+  const templatePath = path.join("media", "circle-template.svg");
+  // Read the file content
+  fs.readFile(templatePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(`Error reading file: ${err.message}`);
+      return;
+    }
+
+    // Replace the placeholder
+    const modifiedContent = data.replace(new RegExp(substr, "g"), color);
+
+    // Define output file path
+    const outputFilePath = path.join(
+      path.dirname(templatePath),
+      `circle-${color}.svg`
+    );
+
+    // Write the modified content to a new file
+    fs.writeFile(outputFilePath, modifiedContent, "utf8", (err) => {
+      if (err) {
+        console.error(`Error writing file: ${err.message}`);
+        return;
+      }
+      console.log(`File saved successfully to: ${outputFilePath}`);
+    });
+  });
+}
